0ebc936317cd01a0a473e27cf971fdca99335cd7
Moaath Alrajab <moaath@alrajab.net>
2022-03-01 10:45:11 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class MainClass {
+	
+	public static void main(String[] args) {
+		// ToDo 5: Fix the error
+		
+		// ToDo 6: Fix the constructor of Student class
+		
+		// ToDo 7: Add a toString method for Student class
+		
+		Student std1= new Student("James", 20);
+		
+		// ToDo 8: Set the gpa of the student using the scanner and user
+		// 			input and then print the output.
+		
+		System.out.println(std1);
+		
+		// ToDo 9: add comments and explain your code
+		
+		// ToDo 10: submit using a pull request.
+	}
+
+}
